da637bc81b2ef62566f724ccbbf0bc7a53d7aba2
Johannes Kristan <Johannes.kristan@bosch.com>
2025-04-27 00:49:40 +0200

    init

---
+++ src/main/java/de/htw_berlin/fb4/ossd/App.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.fb4.ossd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
